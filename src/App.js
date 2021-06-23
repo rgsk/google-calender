dcf990fd11ec1435616d522afc4cf2c4578e2186
Rahul Gupta <guptarahul70322@gmail.com>
2021-06-23 13:16:56 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect } from 'react';
+import styles from './App.module.scss';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(process.env.REACT_APP_SERVER_URL);
+      const data = await response.json();
+      console.log(data);
+    })();
+  }, []);
+  return <div className={styles.app}>App</div>;
 }
 
 export default App;
